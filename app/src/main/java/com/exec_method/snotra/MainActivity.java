0c613be9f85305592179375371a17137bc8da93f
execMethod <neighborcell+github@gmail.com>
2019-09-06 12:56:37 +0900

    プロジェクト作成

---
+++ app/src/main/java/com/exec_method/snotra/MainActivity.java
@@ -0,0 +1,14 @@
+package com.exec_method.snotra;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
